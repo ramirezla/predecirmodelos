6d66a6da9127f1b0aa1b2eebe3654231afef23c2
ramirezla <ramirezluisalberto@hotmail.com>
2024-03-19 15:40:37 -0400

    Actualizacion main.py return list()

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def predecir_modelo(texto:str):
 	# Realizar la predicción utilizando el modelo cargado
 	prediccion = classifier.predict(texto_transformado)
 	
-	return prediccion
+	return list(prediccion)
