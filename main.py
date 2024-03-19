8aa6f4d094c6732a4fd3a11d51fe179f3692d9d3
ramirezla <ramirezluisalberto@hotmail.com>
2024-03-19 18:00:43 -0400

    Actualizacion import string

--- main.py
+++ main.py
@@ -16,10 +16,10 @@
 # transformers==4.18.0<br>
 
 from fastapi import FastAPI
-import pickle
+#import pickle
 #import string
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import LinearSVC
+##from sklearn.feature_extraction.text import CountVectorizer
+##from sklearn.svm import LinearSVC
 # import sys
 
 
@@ -27,26 +27,26 @@ from sklearn.svm import LinearSVC
 # with open('model_tokenizador_descripcion_a_modelos.pkl', 'rb') as f:
 #    classifier = pickle.load(f)
 
-with gzip.open('model_tokenizador_descripcion_a_modelos.pkl.gz', 'rb') as f:
-    train_set, valid_set, test_set = pickle.load(f)
+# with gzip.open('model_tokenizador_descripcion_a_modelos.pkl.gz', 'rb') as f:
+    # train_set, valid_set, test_set = pickle.load(f)
 
-with open('vectorizador_tokenizador_descripcion_a_modelos.pkl', 'rb') as f:
-    real_vectorizer = pickle.load(f)
+# with open('vectorizador_tokenizador_descripcion_a_modelos.pkl', 'rb') as f:
+    # real_vectorizer = pickle.load(f)
 
 # Tokenizar en palabras
-def tokenize(sentence):
-	import string
+# def tokenize(sentence):
+	# import string
 	
-	punctuation = set(string.punctuation)
-	tokens = []
-	for token in sentence.split():
-		new_token = []
-		for character in token:
-			if character not in punctuation:
-				new_token.append(character.lower())
-		if new_token:
-			tokens.append("".join(new_token))
-	return tokens
+	# punctuation = set(string.punctuation)
+	# tokens = []
+	# for token in sentence.split():
+		# new_token = []
+		# for character in token:
+			# if character not in punctuation:
+				# new_token.append(character.lower())
+		# if new_token:
+			# tokens.append("".join(new_token))
+	# return tokens
 
 # Se instancia una variable de tipo FastAPI
 #app = FastAPI(title='ML_predecir_usar_modelo-main', description='Luis A Ramirez G')
@@ -55,7 +55,12 @@ app = FastAPI()
 @app.get('/predecir_modelo/{texto}')
 def predecir_modelo(texto: str):
     try:
+		# Preprocesar el texto de ejemplo utilizando el mismo tokenizador
 		texto_preprocesado="chevrolet ave 4 ptas"
+		# Transformar el texto preprocesado utilizando el vectorizador cargado
+		##texto_transformado=real_vectorizer.transform(texto_preprocesado)
+		# Realizar la predicción utilizando el modelo cargado
+		##prediccion=classifier.predict(texto_transformado)
     except (ValueError, SyntaxError):
         pass 
 	return texto_preprocesado
