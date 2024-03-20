5ce509262e58e2eccfa74c0353351a97dad31856
Luis A, Ramirez G <ramirezluisalberto@hotmail.com>
2024-03-20 11:08:16 -0400

    Update main.py
    Se esta entrenando el modelo

--- main.py
+++ main.py
@@ -10,48 +10,56 @@
 from fastapi import FastAPI
 
 import pickle
-#from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.svm import LinearSVC
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-import sys	
 
-# Se instancia una variable de tipo FastAPI
-#app = FastAPI(title='ML_predecir_usar_modelo-main', description='Luis A Ramirez G')
-
-def cargarModelo():
-	import pickle
+def cargarDatos():
+	import pandas as pd
+	import numpy as np
+	import csv
 	
-	filename = "model_descripcion_a_modelos.pkl"
-	pipeline = pickle.load(open(filename, "rb"))
-	return pipeline
+	# Cargando los datos desde un archivo
+	df_datos = pd.read_csv("Marcas-Modelos_ICR_limpio.csv", encoding='latin-1', sep='|')[['snombremarca','smodelo','MODELO PADRE']]
+	df_datos.dropna()
+	# Modelando los datos
+	df_datos_compacto = df_datos[['MODELO PADRE']]
+	df_datos_compacto = df_datos_compacto.rename(columns={'MODELO PADRE':'label'})
+	df_datos_compacto["text"] = None
+	df_datos_compacto.loc[:,'text'] = df_datos['snombremarca'].astype(str) + ' ' + df_datos['smodelo'].astype(str)	
+	# Creando un arreglo con cada lista de los datos.
+	descripciones = np.array(df_datos_compacto["text"])
+	modelos = np.array(df_datos_compacto["label"])
+	
+	return descripciones, modelos
 
-# Tokenizar en palabras
-# filename = "model_descripcion_a_modelos.pkl"
-# pipeline = pickle.load(open(filename, "rb"))
+descripcion, modelos = cargarDatos()
 
-app = FastAPI()
+# Dividir los datos en conjunto de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(descripcion, modelos, test_size=0.2, random_state=42)
 
-@app.get("/")
-def index():
-    return {"Saludos": "Hola"}
+# Crear el pipeline
+pipeline = Pipeline([
+	('vectorizer', TfidfVectorizer()),  # Vectorización de texto utilizando TF-IDF
+	('classifier', LinearSVC())  		# Clasificador LinearSVC
+])
 
-@app.get("/predecir/{ejemplo}")
-def predecir(ejemplo:str):
-	return ejemplo
+# Entrenar el modelo
+pipeline.fit(X_train, y_train)
 
-@app.get('/predecir_modelo')
-def predecir_modelo():
+# Evaluar el modelo
+y_pred = pipeline.predict(X_test)
+
+app = FastAPI(title='ML_predecir_usar_modelo-main', description='Luis A Ramirez G')
+
+@app.get('/predecir_modelo/{texto}')
+def predecir_modelo(texto:str):
 	try:
-		texto = "chevrolet ave0 lt"
 		cadena = [texto]
-		pipeline = cargarModelo()
-		prediccion = texto
 		predecir = pipeline.predict(cadena)
 	except (ValueError, SyntaxError):
 		pass
-	return (prediccion + " " + predecir)
+	return (list(predecir))
